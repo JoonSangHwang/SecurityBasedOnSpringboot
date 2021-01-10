21d640ab0f9262bcb673ed426baa26ddf7c5c177
joonsang <gufrus@naver.com>
2021-01-10 16:21:01 +0900

    [2021.01.10] 실전 프로젝트 초기 세팅

--- example/src/main/java/com/joonsang/example/ExampleApplication.java
+++ example/src/main/java/com/joonsang/example/ExampleApplication.java
@@ -1,12 +1,9 @@
 package com.joonsang.example;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class ExampleApplication {
 
 	public static void main(String[] args) {
---
+++ example/src/main/java/com/joonsang/example/controller/user/UserController.java
@@ -0,0 +1,15 @@
+package com.joonsang.example.controller.user;
+
+
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class UserController {
+
+}
