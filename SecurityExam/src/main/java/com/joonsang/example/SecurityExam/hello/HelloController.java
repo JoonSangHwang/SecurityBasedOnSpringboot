cd4a79131c2386465d2abd89b8e3c25e32d257b1
joonsang <gufrus@naver.com>
2021-02-05 14:23:17 +0900

    [2021.02.05] 스프링 웹 프로젝트 만들기

---
+++ SecurityExam/src/main/java/com/joonsang/example/SecurityExam/hello/HelloController.java
@@ -0,0 +1,39 @@
+package com.joonsang.example.SecurityExam.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/")
+    public String index(Model model, Principal principal) {
+        if (principal == null) {
+            model.addAttribute("message", "Hello Guest");
+        } else {
+            model.addAttribute("message", "Hello " + principal.getName());
+        }
+        return "index";
+    }
+
+    @GetMapping("/info")
+    public String info(Model model) {
+        model.addAttribute("message", "Info");
+        return "index";
+    }
+
+    @GetMapping("/dashboard")
+    public String dashboard(Model model, Principal principal) {
+        model.addAttribute("message", "dashboard");
+        return "index";
+    }
+
+    @GetMapping("/admin")
+    public String admin(Model model, Principal principal) {
+        model.addAttribute("message", "admin");
+        return "index";
+    }
+}
