5b623bae72aaed131e4be3a2229cafc7d9cd6df7
joonsang <gufrus@naver.com>
2021-02-05 14:10:46 +0900

    [2021.02.05] SecurityExam init

---
+++ SecurityExam/src/test/java/com/joonsang/example/SecurityExam/SecurityExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joonsang.example.SecurityExam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
